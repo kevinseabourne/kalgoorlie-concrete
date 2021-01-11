9dd907b625430f047dbc57e581c6808b47935acc
kevinseabourne <kevinseabourne@hotmail.com>
2021-01-11 12:37:48 +0800

    test

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <Container>
+      <Title>Kalgoorle Precast Concrete</Title>
+    </Container>
+  );
+}
+
+const Container = styled.div``;
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
-
-export default function Home() {
-  return <Title>My page</Title>
-}
+`;
